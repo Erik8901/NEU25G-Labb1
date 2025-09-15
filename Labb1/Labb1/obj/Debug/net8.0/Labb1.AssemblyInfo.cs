4cdb8f4fa7bb52bb9eea6e35d68eb465439f3fc4
Erik8901 <erikjohansson890@gmail.com>
2025-09-15 15:16:06 +0200

    soon

--- Labb1/Labb1/obj/Debug/net8.0/Labb1.AssemblyInfo.cs
+++ Labb1/Labb1/obj/Debug/net8.0/Labb1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Labb1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5676ead089b3d341632d83647ef2d44623d2bb1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b71934dc3ddc3dc998d7c9e89bd6ef60abc30e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Labb1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Labb1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
